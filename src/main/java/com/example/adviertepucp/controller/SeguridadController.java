032941827ab74c915c06f2d7d249759797ff55ac
MarceloJon <mrojasl@pucp.edu.pe>
2022-09-12 23:54:58 -0500

    seguridad 2

--- src/main/java/com/example/adviertepucp/controller/SeguridadController.java
+++ src/main/java/com/example/adviertepucp/controller/SeguridadController.java
@@ -1,2 +1,15 @@
-package com.example.adviertepucp.controller;public class SeguridadController {
+package com.example.adviertepucp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({"/seguridad"})
+public class SeguridadController {
+    @GetMapping("")
+    String listarSeguridad(){
+        return "seguridad/listaMapa";
+    }
+
 }
