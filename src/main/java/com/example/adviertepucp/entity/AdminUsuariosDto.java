fd92efafe21bd3307de638e45e694c3b2ec53e64
caleb <caleb.casapaico@pucp.edu.pe>
2022-09-20 18:06:26 -0500

    lista Users a 30% e Admin...

---
+++ src/main/java/com/example/adviertepucp/entity/AdminUsuariosDto.java
@@ -0,0 +1,11 @@
+package com.example.adviertepucp.entity;
+
+public interface AdminUsuariosDto {
+
+    int getId_ca();
+    String getCodigo();
+    String getNombres();
+    String getCorreo();
+    String getEstado();
+    String getRol();
+}
---
+++ src/main/java/com/example/adviertepucp/repository/AdmiRepository.java
@@ -0,0 +1,21 @@
+package com.example.adviertepucp.repository;
+
+import com.example.adviertepucp.entity.AdminUsuariosDto;
+import com.example.adviertepucp.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AdmiRepository extends JpaRepository<Usuario, Integer> {
+    @Query(value ="SELECT c.idcategoria as id_ca, u.codigo as codigo, concat(u.apellido,' ', u.nombre) as nombres, u.correo as correo,\n" +
+            "u.suspendido as estado,\n" +
+            "c.nombre as rol  FROM usuario u \n" +
+            "inner join categoria c on (u.categoria = c.idcategoria)",
+            nativeQuery = true)
+    List<AdminUsuariosDto> listaUsuariosAdmin();
+
+
+}
