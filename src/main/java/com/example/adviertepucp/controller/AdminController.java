90d6a4052535ee79cc6bc59ff69f18f6492b1350
caleb <caleb.casapaico@pucp.edu.pe>
2022-09-20 18:02:16 -0500

    lista Users a 30% e Admin

--- src/main/java/com/example/adviertepucp/controller/AdminController.java
+++ src/main/java/com/example/adviertepucp/controller/AdminController.java
@@ -1,14 +1,20 @@
 package com.example.adviertepucp.controller;
 
+import com.example.adviertepucp.repository.AdmiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdminController {
+    @Autowired
+    AdmiRepository admiRepository;
     @GetMapping("")
-    public String listaUsuarios(){
+    public String listaUsuarios(Model model){
+        model.addAttribute("listaUsuarios", admiRepository.listaUsuariosAdmin());
         return "admin/listaUsuarios";
     }
 
