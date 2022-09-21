e7b7e082f7b467a1a89706bbea495ab394eee5ca
caleb <caleb.casapaico@pucp.edu.pe>
2022-09-21 14:07:27 -0500

    lista User con un error

--- src/main/java/com/example/adviertepucp/controller/LoguinController.java
+++ src/main/java/com/example/adviertepucp/controller/LoguinController.java
@@ -3,25 +3,27 @@ package com.example.adviertepucp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("")
 public class LoguinController {
     @GetMapping({"/autenticacion"})
     public String autenticacion()
     {
-        return "/loguin/autenticacion";
+        return "loguin/autenticacion";
     }
 
-    @GetMapping({"/registro"})
+    @GetMapping({"registro"})
     public String registro()
     {
-        return "/loguin/registro";
+        return "loguin/registro";
     }
 
-    @GetMapping({"/nuevacontrasena"})
+    @GetMapping({"nuevacontrasena"})
     public String nuevacontrasena()
     {
-        return "/loguin/nuevacontrasena";
+        return "loguin/nuevacontrasena";
     }
 
     @GetMapping({"/restablecercontrasena"})
