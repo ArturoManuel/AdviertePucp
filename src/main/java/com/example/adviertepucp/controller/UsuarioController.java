e72bbb9580243bd6dd31fc5faeb1cad29877b867
MarceloJon <mrojasl@pucp.edu.pe>
2022-09-23 18:32:56 -0500

    problemas navbar

--- src/main/java/com/example/adviertepucp/controller/UsuarioController.java
+++ src/main/java/com/example/adviertepucp/controller/UsuarioController.java
@@ -2,9 +2,14 @@
 
 package com.example.adviertepucp.controller;
 
+import com.example.adviertepucp.entity.Incidencia;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping({"/usuario"})
@@ -39,5 +44,11 @@ public class UsuarioController {
     @GetMapping({"/nuevoIncidente"})
     public String nuevo() {
         return "usuario/nuevoIncidente";
+    }
+    @PostMapping("/guardarincidente")
+    public String guardarIncidente(@RequestParam("archivo") MultipartFile file,
+                                   Incidencia incidencia, Model model){
+
+        return "";
     }
 }
