8b6622c788413928d3a17680020f3dc460fd5541
Jorge Guillen Rivera <jorgegrr052002@gmail.com>
2022-09-25 18:51:17 -0500

    dashboard-seguridad

--- src/main/java/com/example/adviertepucp/controller/SeguridadController.java
+++ src/main/java/com/example/adviertepucp/controller/SeguridadController.java
@@ -1,12 +1,15 @@
 package com.example.adviertepucp.controller;
 
+import com.example.adviertepucp.repository.IncidenciaRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping({"/seguridad"})
 public class SeguridadController {
+    IncidenciaRepository  incidenciaRepository;
     @GetMapping("")
     String listarSeguridad(){
         return "seguridad/listaMapa";
@@ -22,9 +25,30 @@ public class SeguridadController {
         return "seguridad/mapa";
     }
 
-    @GetMapping("/estadisticas")
+
+     @GetMapping("/estadisticas")
     String dashboard(){
+       return "seguridad/dashboard";
+    }
+
+
+    /*
+    @GetMapping("/estadisticas")
+    public String dashboard(Model model) {
+
+        model.addAttribute("num_incidenciasPorMes", incidenciaRepository.incidenciasPorMes());
+        model.addAttribute("num_incidenciasPorAnio", incidenciaRepository.incidenciasPorAnio());
+        model.addAttribute("num_incidenciasAtendidas", incidenciaRepository.incidenciasAtendidas());
+        model.addAttribute("num_ubicacionesPUCP", incidenciaRepository.ubicacionesPUCP());
+        model.addAttribute("num_usuariosReportados", incidenciaRepository.usuariosReportados());
+
+
+        //
+        model.addAttribute("num_totalUsuario", incidenciaRepository.totalUsuarios());
+        model.addAttribute("lista_estadousuario", incidenciaRepository.estadoUsuarios());
         return "seguridad/dashboard";
     }
+
+*/
 
 }
--- src/main/java/com/example/adviertepucp/repository/IncidenciaRepository.java
+++ src/main/java/com/example/adviertepucp/repository/IncidenciaRepository.java
@@ -1,7 +1,9 @@
 package com.example.adviertepucp.repository;
 
 import com.example.adviertepucp.dto.TipoIncidenciadto;
+import com.example.adviertepucp.dto.IncidenciaPorZona;
 import com.example.adviertepucp.entity.Incidencia;
+import com.example.adviertepucp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
@@ -10,5 +12,49 @@ public interface IncidenciaRepository extends JpaRepository<Incidencia, Integer>
     @Query(value = "select " +
             "t.idtipoincidencia as idt , t.nombre as nombret , t.color as colort from tipoincidencia t",nativeQuery = true)
     List<TipoIncidenciadto> listaTipo();
+
+
+
+    //dashboard
+    @Query(value = "select count(idincidencia) from incidencia WHERE fecha > NOW() - INTERVAL 1 MONTH",
+            nativeQuery = true)
+    List<Incidencia> incidenciasPorMes();
+
+    @Query(value = "select count(idincidencia) from incidencia WHERE fecha > NOW() - INTERVAL 12 MONTH",
+            nativeQuery = true)
+    List<Incidencia> incidenciasPorAnio();
+
+    @Query(value = "select count(idincidencia) from incidencia WHERE estado = 'atendido'",
+            nativeQuery = true)
+    List<Incidencia> incidenciasAtendidas();
+
+    //select zp.nombre from incidencia i
+    //inner join zonapucp zp on (zp.idzonapucp = i.zonapucp)
+    @Query(value = "select zp.nombre, count(i.zonapucp) from incidencia i\n" +
+            "inner join zonapucp zp on (zp.idzonapucp = i.zonapucp)\n" +
+            "group by zp.nombre;",
+            nativeQuery = true)
+    List<IncidenciaPorZona> ubicacionesPUCP();
+
+    //select count(codigo) from usuario WHERE suspendido = '1';
+
+    @Query(value = "SELECT  round((count(u.suspendido)*100)/t.total, 2) as 'porcentaje' from usuario u\n" +
+            "CROSS JOIN (SELECT count(codigo)  as total FROM usuario u) t\n" +
+            "where u.suspendido=1",
+            nativeQuery = true)
+    List<Usuario> usuariosReportados();
+
+    @Query(value = "select count(codigo) from usuario",
+            nativeQuery = true)
+    List<Usuario> totalUsuarios();
+
+
+    //lista de todos los estados
+    @Query(value = "SELECT count(codigo),\n" +
+            "CASE WHEN suspendido = 0 THEN 'activo' WHEN suspendido = 1 THEN 'suspendido' ELSE 'no registrado' END AS estado\n" +
+            "FROM adviertedb.usuario\n" +
+            "group by estado;",
+    nativeQuery = true)
+    List<Usuario> estadoUsuarios();
 
 }
